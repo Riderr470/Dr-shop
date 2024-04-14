71ec893a0f5c928bfb51ea24e35dd2ce9f7ec989
Riderr470 <riderr40@gmail.com>
2024-04-14 20:58:53 +0600

    users, categories and products migration created

---
+++ database/migrations/2024_04_14_073230_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('email', 128)->unique();
+            $table->string('phone', 32)->unique();
+            $table->string('password', 128);
+            $table->bigInteger('reward_points')->default(0);
+            $table->string('status');
+            $table->date('email_varified_at')->nullable();
+            $table->string('email_varification_token')->nullable();
+            $table->string('facebook_id', 32)->nullable();
+            $table->string('google_id', 32)->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2024_04_14_143936_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id');
+            $table->string('name', 128);
+            $table->string('slug', 128);
+            $table->longText('description');
+            $table->tinyInteger('in_stock')->default(1);
+            $table->tinyInteger('status')->default(1);
+            $table->decimal('price', 9, 2);
+            $table->decimal('sale_price', 9, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
